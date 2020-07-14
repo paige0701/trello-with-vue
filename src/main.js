2e876879814e1f064cf0a36359fcd9a445245d52
Paige <paigechoi0701@gmail.com>
2020-07-14 16:16:58 +0900

    refactoring router

--- src/main.js
+++ src/main.js
@@ -1,21 +1,5 @@
 import Vue from 'vue'
-import App from './App.vue'
-import VueRouter from 'vue-router';
-
-Vue.use(VueRouter); // 미들웨어라고 한다.
-
-const Login = {template: '<div> Login Page </div>'}
-const NotFound = {template: '<div> Page not found </div>'};
-
-
-const router = new VueRouter({
-  mode: 'history', // url 에 * 없앰
-  routes :[
-    {path: '/', component: App},
-    {path: '/login', component: Login},
-    {path: '*', component: NotFound},
-  ]
-})
+import router from './router/index'
 
 new Vue({
   el: '#app',
