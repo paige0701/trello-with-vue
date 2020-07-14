aa95b4b9b4b1af075cb90147e5562da7d09a11e5
Paige <paigechoi0701@gmail.com>
2020-07-14 16:10:05 +0900

    using vue-router

--- src/main.js
+++ src/main.js
@@ -1,22 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter); // 미들웨어라고 한다.
 
 const Login = {template: '<div> Login Page </div>'}
+const NotFound = {template: '<div> Page not found </div>'};
 
-const routes = {
-  '/': App,
-  '/login': Login
-}
+
+const router = new VueRouter({
+  mode: 'history', // url 에 * 없앰
+  routes :[
+    {path: '/', component: App},
+    {path: '/login', component: Login},
+    {path: '*', component: NotFound},
+  ]
+})
 
 new Vue({
   el: '#app',
-  computed: {
-    VueComponent() {
-      return routes[window.location.pathname] ||
-        {template: '<div> Page not found </div>'}
-    }
-  },
+  router,
   render(h) {
-    return h(this.VueComponent)
+    return h({template: '<router-view></router-view>'})
   }
 })
