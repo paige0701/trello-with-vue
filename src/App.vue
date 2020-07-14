11ad26c1c4b3ecd72d9de4ccf39860a0eea87ee8
Paige <paigechoi0701@gmail.com>
2020-07-14 16:34:42 +0900

    using router link

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <div id="app">
+    <Navbar></Navbar>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+  import Navbar from './components/Navbar'
 export default {
   name: 'app',
+  components: { Navbar },
   data () {
     return {
     }
