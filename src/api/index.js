94e3071e7c7ebf8b15b3500a14ded6a087022214
Paige <paigechoi0701@gmail.com>
2020-07-22 22:37:19 +0900

    delete card item

--- src/api/index.js
+++ src/api/index.js
@@ -47,6 +47,9 @@ export const card = {
   },
   update(id, payload) {
     return request('put', `/cards/${id}`, payload)
+  },
+  delete(id) {
+    return request('delete', `/cards/${id}`)
   }
 }
 
