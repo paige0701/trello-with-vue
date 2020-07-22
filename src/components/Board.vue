6a674c9810fec7cf1bc21cce3071943528d4b7e6
Paige <paigechoi0701@gmail.com>
2020-07-22 16:02:03 +0900

    add drag card function

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -21,6 +21,8 @@
 <script>
   import {mapState, mapActions} from 'vuex'
   import List from './List'
+  import dragula from 'dragula'
+  import 'dragula/dist/dragula.css'
 
   export default {
     components: {
@@ -29,7 +31,8 @@
     data() {
       return {
         bid: 0,
-        loading: false
+        loading: false,
+        dragulaCards: null
       }
     },
     computed: {
@@ -43,7 +46,7 @@
     },
     methods: {
       ...mapActions([
-        'FETCH_BOARD'
+        'FETCH_BOARD', 'UPDATE_CARD'
       ]),
       fetchData() {
         this.loading = true
@@ -52,6 +55,42 @@
         })
 
       }
+    },
+    updated() {
+      if (this.dragulaCards) this.dragulaCards.destroy()
+      this.dragulaCards = dragula([
+        ...Array.from(this.$el.querySelectorAll('.card-list'))
+      ]).on('drop', (el, wrapper, target, siblings) => {
+        const targetCard = {
+          id: el.dataset.cardId * 1,
+          pos: 65535
+        }
+        let prevCard = null
+        let nextCard = null
+
+        Array.from(wrapper.querySelectorAll('.card-item'))
+          .forEach((el, idx, arr) => {
+          const cardId = el.dataset.cardId * 1
+          if (cardId == targetCard.id) {
+            prevCard = idx > 0 ? {
+              id: arr[idx-1].dataset.cardId * 1,
+              pos: arr[idx-1].dataset.cardPos * 1
+            } : null
+            nextCard = idx < arr.length -1? {
+              id: arr[idx+1].dataset.cardId * 1,
+              pos: arr[idx+1].dataset.cardPos * 1
+            } : null
+          }
+        })
+        if (!prevCard && nextCard) targetCard.pos = nextCard.pos / 2
+        else if (!nextCard && prevCard) targetCard.pos = prevCard.pos * 2
+        else if (prevCard && nextCard) targetCard.pos = (prevCard.pos + nextCard.pos) /2
+
+        console.info(targetCard)
+        this.UPDATE_CARD(targetCard)
+      })
+
+
     }
   }
 
